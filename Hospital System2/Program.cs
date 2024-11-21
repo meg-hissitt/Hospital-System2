115bce729e85e148902e7359968a40cb399112d2
hissi <hissi@MSI>
2024-11-21 10:33:57 +0000

    aaaa

--- Hospital System2/Program.cs
+++ Hospital System2/Program.cs
@@ -9,86 +9,46 @@
         {
             public string Username { get; set; }
             public string Password { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string PatientID { get; set; }
+
         }
-        static List<User> users = new List<User>();
 
-        static string usersFilePath = @"users.json";
-        static void Main(string[] args)
+        string usersFilePath = @"users.json";
+        public static void Main(string[] args)
         {
-            LoadUsers();
+            UserManager uman = new UserManager();
 
-            Console.WriteLine("1. Login");
-            Console.WriteLine("2. Register");
-            Console.Write("What would you like to do? ");
+            uman.LoadUsers();
 
-            string choice = Console.ReadLine();
+            bool running = true;
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Register");
+                Console.Write("What would you like to do? ");
 
-            if (choice == "1")
-            {
-                Login();
-            }
-            else if (choice == "2")
-            {
-                Register();
-            }
-            else
-            {
-                Console.WriteLine("Invalid input");
-            }
-        }
-        static void LoadUsers()
-        {
-            if (File.Exists(usersFilePath))
-            {
-                string json = File.ReadAllText(@"users.json");
-            }
-        }
-        static void SaveUsers()
-        {
-            string json = JsonConvert.SerializeObject(users);
-            File.WriteAllText(@"users.json", json);
-        }
-        static void Login()
-        {
-            Console.Write("Username: ");
-            string username = Console.ReadLine();
-            Console.Write("Password: ");
-            string password = Console.ReadLine();
-            var user = users.Find(u => u.Username == username && u.Password == password);
+                string choice = Console.ReadLine();
 
-            if (user != null)
-            {
-                Console.WriteLine("Login successfull.");
-            }
-            else
-            {
-                Console.WriteLine("Login unsuccesfull");
+                if (choice == "1")
+                {
+                    uman.Login();
+                }
+                else if (choice == "2")
+                {
+                    uman.Register();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
             }
         }
-        static void Register()
-        {
-            Console.Write("Enter your first and last name: ");
-            string name = Console.ReadLine();
 
-            Console.Write("Enter your address: ");
-            string address = Console.ReadLine();
-
-            Console.Write("Enter your patient number: ");
-            string patienID = Console.ReadLine();
-
-            Console.Write("Create a username: ");
-            string username = Console.ReadLine();
-
-            Console.Write("Create a password: ");
-            string password = Console.ReadLine();
-
-            User newUser = new User { Username = username, Password = password };
-            users.Add(newUser);
-            SaveUsers();
         }
     }
 
 
-
-}
 
---
+++ Hospital System2/UserManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using static Hospital_System2.Program;
+
+namespace Hospital_System2
+{
+    class UserManager
+    {
+        public string json { get; set; }
+        public void LoadUsers()
+        {
+            if (File.Exists(@"users.json"))
+            {
+                json = File.ReadAllText(@"users.json");
+            }
+            else
+            {
+                File.Create(@"users.json").Close();
+                json = File.ReadAllText(@"users.json");
+            }
+
+        }
+        public void Login()
+        {
+            LoadUsers();
+            List<User> userList = JsonConvert.DeserializeObject<List<User>>(this.json);
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+            foreach (var user in userList)
+            {
+                if (user.Username == username && user.Password == password)
+                {
+                    Console.WriteLine("Login successfull.");
+                }
+                else
+                {
+                    Console.WriteLine("Login unsuccesfull");
+                }
+            }
+
+        }
+        void SaveUsers(List<User> users)
+        {
+            string json = JsonConvert.SerializeObject(users);
+            File.WriteAllText(@"users.json", json);
+        }
+        public void Register()
+        {
+            Console.Write("Enter your first and last name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter your patient number: ");
+            string patientID = Console.ReadLine();
+
+            Console.Write("Create a username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Create a password: ");
+            string password = Console.ReadLine();
+
+            List<User> userList = JsonConvert.DeserializeObject<List<User>>(this.json);
+            User newUser = new User {Name = name, Address = address, PatientID = patientID, Username = username, Password = password };
+            userList.Add(newUser);
+            SaveUsers(userList);
+        }
+    }
+}
